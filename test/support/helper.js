0ee293da0b940b036392ec249bcbf5e577ba6abb
Josh Rowley <joshua.f.rowley@gmail.com>
2015-04-03 13:07:11 -0400

    test that tokens are loaded into the correct places

--- test/support/helper.js
+++ test/support/helper.js
@@ -2,10 +2,9 @@ var chai   = require('chai'),
     path   = require('path'),
     Sprout = require('sprout');
 
-var should = chai.should();
 var sprout = new Sprout(path.join(__dirname, '../config'));
 
 global.chai   = chai;
 global.sprout = sprout;
-global.should = should;
+global.expect = chai.expect;
 global._path  = path.join(__dirname, '../fixtures');
